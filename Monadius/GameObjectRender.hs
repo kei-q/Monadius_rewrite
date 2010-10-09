0d180ecc3f3e8ef749a6e327f235b3c5c0c85483
keqh <keqh.keqh@gmail.com>
2010-10-09 12:17:16 +0900

    refactoring

--- Monadius/GameObjectRender.hs
+++ Monadius/GameObjectRender.hs
@@ -71,12 +71,15 @@ renderMonadius (Monadius (variables,objects)) = do
       h=20
       box = [(0,0),(w,0),(w,h),(0,h)]
       cross = [(0,0),(w,h),(w,0),(0,h)]
+      
+      renderFrame ugoF x l = preservingMatrix $ do
+        translate1D x
+        renderPrimitive LineLoop $ ugoF box
+        when l $ renderPrimitive Lines $ ugoF cross
+
       renderNormal x l i = preservingMatrix $ do
         color $ Color3 0.7 0.8 0.8
-        preservingMatrix $ do
-          translate1D x
-          renderPrimitive LineLoop $ ugoVertices2D 0 1 box
-          when l $ renderPrimitive Lines $ ugoVertices2D 0 1 cross
+        renderFrame (ugoVertices2D 0 1) x l
         preservingMatrix $ do
           ugoTranslate x 0 0 3
           translate1D (w/2)
@@ -86,10 +89,7 @@ renderMonadius (Monadius (variables,objects)) = do
 
       renderActive x l i = preservingMatrix $ do
         color $ Color3 1 1 0
-        preservingMatrix $ do
-          translate1D x
-          renderPrimitive LineLoop $ ugoVertices2DFreq 0 5 2 box
-          when l $ renderPrimitive Lines $ ugoVertices2DFreq 0 5 2 cross
+        renderFrame (ugoVertices2DFreq 0 5 2) x l 
         preservingMatrix $ do
           ugoTranslateFreq x 0 0 5 2
           translate1D (w/2)
@@ -104,35 +104,38 @@ renderMonadius (Monadius (variables,objects)) = do
         scale 0.15 0.13 0.15
         renderString Roman $ ["SPEED","MISSILE","DOUBLE","LASER","OPTION","  ?"]!!i
 
-  renderGameObject vic@VicViper{position = x:+y} = if hp vic<=0 then preservingMatrix $ do
-      translate2D x y
-      scale pishaMagnitudeX pishaMagnitudeY 0
-      renderWithShade (Color3 1.0 0 0) (Color3 1.0 0.6 0.4) $ do
-        renderPrimitive LineLoop $ ugoVertices2DFreq 0 1 1
-          [(0,12),(8,8),(10,4),(20,0),(10,-4),(8,-8),(0,-12),(-8,-8),(-10,-4),(-20,0),(-10,4),(-8,8)]
-    else preservingMatrix $ do
-      translate $ Vector3 x y 0
-      renderWithShade (Color3 1.0 1.0 1.0) (Color3 0.4 0.4 0.6) $ do
-        renderPrimitive LineStrip $ ugoVertices2D 0 2
-          [((-14),(-1)),((-12),5),((-20),13),(-14,13),(2,5),(8,1),(32,1),(32,(-1)),(24,(-3)),(16,(-3))]
-        renderPrimitive LineStrip $ ugoVertices2D 0 2
-          [((-10),(-1)),(14,(-1)),(18,(-5)),(4,(-9)),((-2),(-9))]
-        renderPrimitive LineLoop $ ugoVertices2D 0 2
-          [((-18),3),((-16),3),((-16),(-3)),((-18),(-3))]
-      renderWithShade (Color3 0.92 0.79 0.62) (Color3 0.75 0.38 0.19) $ do
-        renderPrimitive LineStrip $ ugoVertices2D 0 2
-          [(4,3),(6,5),(14,5),(22,1)] --cockpit
-      renderWithShade (Color3 0.6 0.8 1.0) (Color3 0.19 0.38 0.75) $ do
-        renderPrimitive LineLoop $ ugoVertices2D 0 2
-          [((-14),(-1)),((-10),(-1)),((-2),(-9)),((-4),(-9)),((-10),(-7)),((-14),(-3))] -- identification blue coting
-      renderWithShade (Color3 0 0 0.8) (Color3 0.0 0.0 0.4) $ do
-        renderPrimitive LineLoop $ ugoVertices2D 0 4
-          [((-36),1),((-28),5),((-24),5),((-20),1),((-20),(-1)),((-24),(-5)),((-28),(-5)),((-36),(-1))] -- backfire
+  renderGameObject vic@VicViper{position = x:+y} 
+    | hp vic<=0 = renderBang 
+    | otherwise = renderBody
     where
-      pishaMagnitudeX :: Double
-      pishaMagnitudeY :: Double
-      pishaMagnitudeX = max 0 $ (8*) $ (\z -> z*(1-z)) $ (/20) $ intToDouble $ ageAfterDeath vic
-      pishaMagnitudeY = max 0 $ (5*) $ (\z -> z*(1-z)) $ (/15) $ intToDouble $ ageAfterDeath vic
+      renderBang = preservingMatrix $ do
+        translate2D x y
+        scale pishaMagnitudeX pishaMagnitudeY 0
+        renderWithShade (Color3 1.0 0 0) (Color3 1.0 0.6 0.4) $ do
+          renderPrimitive LineLoop $ ugoVertices2DFreq 0 1 1
+            [(0,12),(8,8),(10,4),(20,0),(10,-4),(8,-8),(0,-12),(-8,-8),(-10,-4),(-20,0),(-10,4),(-8,8)]
+        where
+          pishaMagnitudeX :: Double
+          pishaMagnitudeY :: Double
+          pishaMagnitudeX = max 0 $ (8*) $ (\z -> z*(1-z)) $ (/20) $ intToDouble $ ageAfterDeath vic
+          pishaMagnitudeY = max 0 $ (5*) $ (\z -> z*(1-z)) $ (/15) $ intToDouble $ ageAfterDeath vic
+      renderBody = preservingMatrix $ do
+        translate $ Vector3 x y 0
+        renderWithShade (Color3 1.0 1.0 1.0) (Color3 0.4 0.4 0.6) $ do
+          renderLine 0 2 [((-14),(-1)),((-12),5),((-20),13),(-14,13),(2,5),(8,1),(32,1),(32,(-1)),(24,(-3)),(16,(-3))]
+          renderLine 0 2 [((-10),(-1)),(14,(-1)),(18,(-5)),(4,(-9)),((-2),(-9))]
+          renderLine 0 2 [((-18),3),((-16),3),((-16),(-3)),((-18),(-3))]
+        renderWithShade (Color3 0.92 0.79 0.62) (Color3 0.75 0.38 0.19) $ do
+          renderLine 0 2 vCockpit
+        renderWithShade (Color3 0.6 0.8 1.0) (Color3 0.19 0.38 0.75) $ do
+          renderLine 0 2 vCoting
+        renderWithShade (Color3 0 0 0.8) (Color3 0.0 0.0 0.4) $ do
+          renderLine 0 4 vBackfire
+        where
+          renderLine x y vs = renderPrimitive LineLoop $ ugoVertices2D x y vs
+          vCockpit = [(4,3),(6,5),(14,5),(22,1)] --cockpit
+          vCoting = [((-14),(-1)),((-10),(-1)),((-2),(-9)),((-4),(-9)),((-10),(-7)),((-14),(-3))] -- identification blue coting
+          vBackfire = [((-36),1),((-28),5),((-24),5),((-20),1),((-20),(-1)),((-24),(-5)),((-28),(-5)),((-36),(-1))] -- backfire
 
   renderGameObject Option{position = x:+y} = preservingMatrix $ do
     translate2D x y
